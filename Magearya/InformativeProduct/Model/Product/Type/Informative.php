ffdb39e57ed06d0e07049d56c7a9c34aa67a3795
Birjitsinh Zala <45305512+Birjitsinh@users.noreply.github.com>
2022-04-26 15:23:05 +0530

    Add files via upload

---
+++ Magearya/InformativeProduct/Model/Product/Type/Informative.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Magearya
+ *
+ * @category    Magearya
+ * @package     Magearya_InformativeProduct
+ */
+namespace Magearya\InformativeProduct\Model\Product\Type;
+
+class Informative extends \Magento\Catalog\Model\Product\Type\AbstractType
+{
+    public function deleteTypeSpecificData(\Magento\Catalog\Model\Product $product)
+    {
+        // TODO: Implement deleteTypeSpecificData() method.
+    }
+}
---
+++ Magearya/InformativeProduct/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Magearya
+ *
+ * @category    Magearya
+ * @package     Magearya_InformativeProduct
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Magearya_InformativeProduct',
+    __DIR__
+);
